683ef64e0203915925fe6755275fdc5bafbec490
Elias Karakoulakis <elias.karakoulakis@gmail.com>
2014-07-07 22:24:50 +0300

    added licence + headers

--- 10-zwave.js
+++ 10-zwave.js
@@ -1,3 +1,22 @@
+/*
+
+  OpenZWave nodes for IBM's Node-Red
+  https://github.com/ekarak/node-red-contrib-openzwave
+  (c) 2014, Elias Karakoulakis <elias.karakoulakis@gmail.com>
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+  
+*/
 module.exports = function(RED) {
 	console.log("loading openzwave for node-red");
     var OZW = require('openzwave');
